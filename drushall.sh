a9deb84e7aee2cecce1fd2e4c55ea87b00774097
Bryan Green <bcgreen@gmail.com>
2013-01-10 21:44:42 -0800

    initial commit

---
+++ drushall.sh
@@ -0,0 +1,9 @@
+for dir in ~/htdocs/drupal/drupal/sites/*
+  do
+    if [ $dir = "/local/users/drupadm/htdocs/drupal/drupal/all" ]
+    then
+      continue
+    fi
+    echo $dir
+    (cd $dir && /opt/webstack/php/5.2/bin/php /local/users/drupadm/drush/drush.php $1)
+  done
