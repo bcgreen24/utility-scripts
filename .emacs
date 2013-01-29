6cd3333ccefde574fa9286ef77512593d0a900d5
bgreen4 <bgreen4@ucmerced.edu>
2013-01-29 00:03:05 -0800

    added emacs config

---
+++ .emacs
@@ -0,0 +1,33 @@
+(put 'downcase-region 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (deeper-blue))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key "\C-c\C-m" 'execute-extended-command)
+(global-set-key "\C-w" 'backward-kill-word)
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(add-to-list 'load-path "/home/bcgreen/emacs/")
+(require 'frame-cmds)
+(tile-frames-vertically)
+(split-window-horizontally)
+(split-window-vertically)
+(defun fullscreen (&optional f)
+  (interactive)
+  (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
+		 '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0))
+  (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
+		 '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0)))
+(fullscreen)
+
+
--- merge_crops_sites.py
+++ merge_crops_sites.py
@@ -4,9 +4,9 @@ from xml.etree import ElementTree as ET
 root = ET.Element("beans")
 dictCourseList = {}
 
-class Handler:
+class Crops_Config:
 
-    def indent(self, elem, level=0):
+    def prettify(self, elem, level=0):
         i = "\n" + level*"  "
         if len(elem):
             if not elem.text or not elem.text.strip():
@@ -14,7 +14,7 @@ class Handler:
             if not elem.tail or not elem.tail.strip():
                 elem.tail = i
             for elem in elem:
-                self.indent(elem, level+1)
+                self.prettify(elem, level+1)
             if not elem.tail or not elem.tail.strip():
                 elem.tail = i
         else:
@@ -22,48 +22,47 @@ class Handler:
                 elem.tail = i
 
     def build_file(self, *args):
-    	bean = ET.SubElement(root, "bean")
-    	bean.attrib["id"] = txtBeanID.get_text()
-    	bean.attrib["class"] = "edu.ucmerced.banner.api.BannerSiteInstructions"
-    	bean.attrib["parent"] = "customCourseSiteDefaults"
-    	if chkTesting.get_active() == 1:
-    		theproperty = ET.SubElement(bean, "property")
-    		theproperty.attrib["name"] = "testing"
-    		theproperty.attrib["value"] = "true"
-    	theproperty = ET.SubElement(bean, "property")
-    	theproperty.attrib["name"] = "custom"
-    	theproperty.attrib["value"] = "true"
-    	#term code
-    	theproperty = ET.SubElement(bean, "property")
-    	theproperty.attrib["name"] = "termCode"
-    	theproperty.attrib["value"] = txtTermCode.get_text()
-    	#site id
-    	theproperty = ET.SubElement(bean, "property")
-    	theproperty.attrib["name"] = "siteId"
-    	theproperty.attrib["value"] = '${termcode}-' + txtSiteID.get_text()
-    	#site title
-    	theproperty = ET.SubElement(bean, "property")
-    	theproperty.attrib["name"] = "siteTitle"
-    	theproperty.attrib["value"] = '${termcode}-' + txtSiteTitle.get_text()
-    	#short description
-    	theproperty = ET.SubElement(bean, "property")
-    	theproperty.attrib["name"] = "shortDescription"
-    	theproperty.attrib["value"] = '${termcode}-' + txtSiteDescription.get_text()
-    	#emailAlias
-    	theproperty = ET.SubElement(bean, "property")
-    	theproperty.attrib["name"] = "emailAlias"
-    	theproperty.attrib["value"] = '${termcode}-' + txtSiteID.get_text()
-    	#section name
-    	theproperty = ET.SubElement(bean, "property")
-    	theproperty.attrib["name"] = "sectionName"
-    	theproperty.attrib["value"] = '${subjectcode}-${coursenumber}-${sectiontypedesc}-${sectionid}'
-    	theproperty.attrib["name"] = "sections"
+        bean = ET.SubElement(root, "bean")
+        bean.attrib["id"] = txtBeanID.get_text()
+        bean.attrib["class"] = "edu.ucmerced.banner.api.BannerSiteInstructions"
+        bean.attrib["parent"] = "customCourseSiteDefaults"
+        if chkTesting.get_active() == 1:
+            theproperty = ET.SubElement(bean, "property")
+            theproperty.attrib["name"] = "testing"
+            theproperty.attrib["value"] = "true"
+        theproperty = ET.SubElement(bean, "property")
+        theproperty.attrib["name"] = "custom"
+        theproperty.attrib["value"] = "true"
+        #term code
+        theproperty = ET.SubElement(bean, "property")
+        theproperty.attrib["name"] = "termCode"
+        theproperty.attrib["value"] = txtTermCode.get_text()
+        #site id
+        theproperty = ET.SubElement(bean, "property")
+        theproperty.attrib["name"] = "siteId"
+        theproperty.attrib["value"] = '${termcode}-' + txtSiteID.get_text()
+        #site title
+        theproperty = ET.SubElement(bean, "property")
+        theproperty.attrib["name"] = "siteTitle"
+        theproperty.attrib["value"] = '${termcode}-' + txtSiteTitle.get_text()
+        #short description
+        theproperty = ET.SubElement(bean, "property")
+        theproperty.attrib["name"] = "shortDescription"
+        theproperty.attrib["value"] = '${termcode}-' + txtSiteDescription.get_text()
+        #emailAlias
+        theproperty = ET.SubElement(bean, "property")
+        theproperty.attrib["name"] = "emailAlias"
+        theproperty.attrib["value"] = '${termcode}-' + txtSiteID.get_text()
+        #section name
+        theproperty = ET.SubElement(bean, "property")
+        theproperty.attrib["name"] = "sectionName"
+        theproperty.attrib["value"] = '${subjectcode}-${coursenumber}-${sectiontypedesc}-${sectionid}'
+        theproperty.attrib["name"] = "sections"
         thelist = ET.SubElement(theproperty, "list")
         #loop through sections and add them  
-    	for key, value in dictCourseList.iteritems():
+        for key, value in dictCourseList.iteritems():
             thevalue = ET.SubElement(thelist, "value")
             thevalue.text = 'subjcode=' + key + ',crsenumb=' + value
-
 
     def exitProgram(self, *args):
         Gtk.main_quit(*args)
@@ -77,18 +76,34 @@ class Handler:
         
     def on_btnWrite_released(self, *args):
         self.build_file()
+        self.prettify(root)
         ET.ElementTree(root).write("/home/bcgreen/course.xml")
-
 
     def on_btnPreview_released(self, *args):
         self.build_file()
-        self.indent(root)
+        self.prettify(root)
         ET.dump(root)
 
-builder = Gtk.Builder()
-builder.add_from_file("hellobox.glade")
-builder.connect_signals(Handler())
+    def on_btnCopySiteID_released(self, *args):
+        txtBeanID.set_text(txtSiteID.get_text())
+        txtBeanID.set_text(txtSiteID.get_text())
+        txtSiteTitle.set_text(txtSiteID.get_text())
+        txtSiteDescription.set_text(txtSiteID.get_text())
+        txtEmailAlias.set_text(txtSiteID.get_text())
+    
+    def on_btnClearAll_released(self, *args):
+        txtSiteID.set_text("")
+        txtSubjCode.set_text("")
+        txtCourseNum.set_text("")
+        txtBeanID.set_text("")
+        txtTermCode.set_text("")
+        txtSiteTitle.set_text("")
+        txtSiteDescription.set_text("")
+        txtEmailAlias.set_text("")
 
+builder = Gtk.Builder()
+builder.add_from_file("merge_crops_sites.glade")
+builder.connect_signals(Crops_Config())
 window = builder.get_object("WindowMain")
 txtSiteID = builder.get_object("txtSiteID")
 txtCourseList = builder.get_object("txtCourseList")
@@ -100,6 +115,9 @@ txtTermCode = builder.get_object("txtTermCode")
 txtSiteTitle = builder.get_object("txtSiteTitle")
 txtSiteDescription = builder.get_object("txtSiteDescription")
 chkTesting = builder.get_object("chkTesting")
+btnCopySiteID = builder.get_object("btnCopySiteID")
+btnClearAll = builder.get_object("btnClearAll")
+txtEmailAlias = builder.get_object("txtEmailAlias")
 window.show_all()
 
 Gtk.main()
