813b2a8b6891550c0b95522ebcd1def5b2e27fc8
bgreen4 <bgreen4@ucmerced.edu>
2013-02-05 13:13:49 -0800

    Edited gitpull.sh to include engineering dev site

--- gitpull.sh
+++ gitpull.sh
@@ -5,7 +5,15 @@ date >> /home/o1.ftp/gitpull.txt
 exit
 cd /data/disk/o1/static/campuscms/campuscms-1.01.20130103-dev/sites/ssha.ucmerced.edu
 drush cc all >> /home/o1.ftp/gitpull.txt
+exit
 
+cd /data/disk/o1/static/campuscms/campuscms-1.01.20130103-dev/sites/engineering.campuscms.ucmerced.edu/themes/cms-theme-inception
+git pull > /home/o1.ftp/gitpull.txt
+date >> /home/o1.ftp/gitpull.txt
+exit
+cd /data/disk/o1/static/campuscms/campuscms-1.01.20130103-dev/sites/engineering.campuscms.ucmerced.edu
+drush cc all >> /home/o1.ftp/gitpull.txt
+exit
 
 
 
