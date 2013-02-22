3bf6b2299d8997350604efe47345028e88921c57
bgreen4 <bgreen4@ucmerced.edu>
2013-02-21 20:48:41 -0800

    added in revised drushall script

--- drushall.sh
+++ drushall.sh
@@ -1,9 +1,24 @@
 for dir in ~/htdocs/drupal/drupal/sites/*
-  do
-    if [ $dir = "/local/users/drupadm/htdocs/drupal/drupal/sites/all" ]
-    then
-      continue
-    fi
-    echo $dir
-    (cd $dir && /opt/webstack/php/5.2/bin/php /local/users/drupadm/drush/drush.php $1)
-  done
+do
+    #-----------------------------------
+    # Derek's new code
+    #-----------------------------------
+    echo ' * * * Working in ' $dir
+    (cd $dir && /opt/webstack/php/5.2/bin/php /local/users/drupadm/drush/drush.php "$@")
+
+    # # Test to make sure arg expansion works as expected
+    # for arg in "$@"
+    # do
+    #     echo ${arg}
+    # done
+
+    #-----------------------------------
+    # Bryan's original code
+    #-----------------------------------
+    # if [ $dir = "/local/users/drupadm/htdocs/drupal/drupal/sites/all" ]
+    # then
+    #   continue
+    # fi
+    # (cd $dir && /opt/webstack/php/5.2/bin/php /local/users/drupadm/drush/drush.php $1)
+done
+
